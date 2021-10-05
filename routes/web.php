f33eca6d17c11b89af3a5f3c53eaf27ad4afc8fb
mehmetsario <mohamad.sario@ogr.iuc.edu.tr>
2021-10-05 17:26:39 +0300

    Updatting Wep.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 
 Route::get('/', function () {
     return view('dashboard');
